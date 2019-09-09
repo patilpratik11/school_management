7044e1b59aafd51267afcc868e911a34d199bc6e
saurabh kyatam <saurabhkyatam77@gmail.com>
2019-09-09 15:36:44 +0530

    Student model updated

--- student/models.py
+++ student/models.py
@@ -1,12 +1,53 @@
 from django.db import models
 
 # Create your models here.
-class Student_information(models.Model):
-    sid = models.CharField(max_length=20)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    class Meta:
-        db_table = "student_info"
 
+class Teacher(models.Model):
+	teacher_id = models.AutoField(primary_key=True) 
+	first_name = models.CharField(max_length=100)
+	middle_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	email = models.EmailField(max_length=200, unique=True)
+	password = models.CharField(max_length=100)
+	mobile = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.first_name
 
+	def getPassword(self):
+		return self.password
+
+	def getEmail(self):
+		return self.email
+
+	def getfName(self):
+		return self.first_name
+
+	def getlName(self):
+		return self.last_name
+
+	def getMobile(self):
+		return self.mobile
+
+class ClassTeacher(models.Model):
+	#foreign key teacher_id
+	teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+	class_name = models.CharField(max_length=100, primary_key=True)
+
+	def __str__(self):
+ 		return self.class_name;
+
+class Subject(models.Model):
+	#primary key subject_id
+	subject_id = models.AutoField(primary_key=True)	
+	subject_name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+ 		return self.subject_name;
+
+class SubjectTeacher(models.Model):
+	#foreign key teacher_id subject_id class
+
+	teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+	subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+	class_name = models.ForeignKey(ClassTeacher, on_delete=models.CASCADE)
--- student/views.py
+++ student/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from sms.models import Teacher
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'common/index.html') 
+
+def acctype(request):
+    return render(request, 'common/accounts.html')    
+
+def studentLogin(request):
+    return render(request, 'common/login_student.html')    
+
+def teach_Login(request):
+    id = 1
+    return render(request, 'common/teach_parent_admin_login.html', {"id" : id })
+
+def parent_Login(request):
+    id = 2
+    return render(request, 'common/teach_parent_admin_login.html', {"id" : id })
+
+def admin_Login(request):
+    id = 3
+    return render(request, 'common/teach_parent_admin_login.html', {"id" : id })    
+
+def loginTeacher(request):
+	
+	email_id = request.POST["email"]
+	password = request.POST["password"]
+
+	query = Teacher.objects.get(email=email_id)
+
+	if password == query.getPassword():
+		first_name = query.getfName()
+		last_name = query.getlName()
+		return render(request, 'common/success.html', {'first':first_name, 'last':last_name})
+
+	return render(request, 'common/failed.html')
