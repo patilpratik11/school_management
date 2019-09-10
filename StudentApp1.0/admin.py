ecf5ebb81caf9fb30cdae6cf905664cdc508d30a
Shanty97 <shantanu1612@gmail.com>
2019-09-10 16:27:15 +0530

    Student Initial

---
+++ StudentApp1.0/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import StudentModell, ClassModell, ParentModell, Student_attendence, Student_marks, SubjectModell, ExamModell
+
+# Register your models here.
+admin.site.register(StudentModell)
+admin.site.register(ClassModell)
+admin.site.register(ParentModell)
+admin.site.register(Student_attendence)
+admin.site.register(Student_marks)
+admin.site.register(SubjectModell)
+admin.site.register(ExamModell)
