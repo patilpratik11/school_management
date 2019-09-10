0bcf8cab35b5e3ea854aa810a3f483248b3518bc
patilpratik11 <meet.urpratik@gamil.com>
2019-09-10 20:05:28 +0530

    Removed Student Application

--- student/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from student.models import Student_information
-
-
-class Student_information_form(forms.ModelForm):
-    class Meta:
-        model = Student_information
-        fields = {"first_name","middle_name","last_name"}
-
